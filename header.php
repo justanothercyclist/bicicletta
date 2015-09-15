fd7d4ce95a5e8807eb26cb5039a71e9b06437f57
Ross Del Duca <ross@veloreviews.com>
2015-09-14 21:28:53 -0700

    Simple start. Basic header working. Panels mostly in place. No JS yet.

---
+++ header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>Tutorial theme</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+
+</head>
+<body>
+<div id="site-header">
+	<div id="site-header-logo">
+	
+	</div>
+	<h1><?php echo get_bloginfo( $show, $filter ); ?></h1>
+</div>
+<div id="wrapper">
